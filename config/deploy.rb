192515c451397e54bcba2849dc1b597f15f1e621
483316509@qq.com <483316509@qq.com>
2015-09-17 09:46:56 +0800

    first

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, 'myapp'
+
+set :repo_url, 'git@github.com:lyzxsc/myapp.git'
+
+set :deploy_to, '/home/wanglihua/myapp'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :tmp_dir,"/home/wanglihua/tmp"
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '2fa442e4d05c3dea5ed2cb751052af5a0f54ade6c86692eeb399202d44df0d54de7aace9000d7ab82713f0facb2df542c39d79c32cbf4a00ade01f3691384bad'
